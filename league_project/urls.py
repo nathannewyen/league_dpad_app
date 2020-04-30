a9c9ba202761564dba0277d933ee654308ef3c12
David Padilla <dpadman2@gmail.com>
2020-04-29 23:18:20 -0500

    dp refresh

--- league_project/urls.py
+++ league_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('league_app.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
